8cf574efe49f7bc51034092af70c074632c9bc80
WalkerOfBacon <50679909+WalkerOfBacon@users.noreply.github.com>
2024-06-05 20:27:13 -0400

    Fix Server-HideChatCommands.lua
    three variables weren't defined

--- Server/Server-HideChatCommands.lua
+++ Server/Server-HideChatCommands.lua
@@ -7,20 +7,16 @@
 --]]
 --[[
     MIT License
-
     Copyright (c) 2016-2024 Sceleratis (https://github.com/Sceleratis), Epix Incorporated, and the Adonis Community.
     Copyright (c) 2024 ccuser44/ALE111_boiPNG
-
     Permission is hereby granted, free of charge, to any person obtaining a copy
     of this software and associated documentation files (the "Software"), to deal
     in the Software without restriction, including without limitation the rights
     to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
     copies of the Software, and to permit persons to whom the Software is
     furnished to do so, subject to the following conditions:
-
     The above copyright notice and this permission notice shall be included in all
     copies or substantial portions of the Software.
-
     THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
     IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
     FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -29,11 +25,9 @@
     OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
     SOFTWARE.
 --]]
-
 return function(Vargs)
 	local server, service = Vargs.Server, Vargs.Service
-	local Admin, Remote = server.Admin, server.Remote
-
+	local Admin, Remote, Core, Settings, Variables = server.Admin, server.Remote, server.Core, server.Settings, server.Variables
 	Admin.DoHideChatCmd = function(p: Player, message: string, data: {[string]: any}?)
 		local hideCommands = (data or Core.GetPlayer(p)).Client.HideChatCommands
 		if hideCommands == nil or hideCommands == true then
@@ -51,7 +45,6 @@ return function(Vargs)
 			end
 		end
 	end
-
 	service.HookEvent("CharacterAdded", function(player: Player)
 		--// Doesn't save unless sent to server by player
 		if player.Client.HideChatCommands == nil then
